b3bafaf87e1a211bd384a6b0694834eceef1bdca
5930213021 <38065206+5930213021@users.noreply.github.com>
2019-08-11 15:58:01 +0700

    fiinsh login page

--- Login.php
+++ Login.php
@@ -1,29 +1,67 @@
-<?php
-include  'config.php';
-session_start();
+<html lang="en">
 
-$sql = 'SELECT * FROM loginweb WHERE Username ="'.$_POST['username'].'" AND Password = "'.$_POST['password'].'"'; 
-$result = mysqli_query($connect,$sql);
-$numrows = mysqli_num_rows($result);
-$objResult = mysqli_fetch_array($result,MYSQLI_ASSOC);
-if($numrows==0){
- 
- echo "  <script>
-		alert('กรุณาตรวจสอบ ชื่อผู้ใช้หรือรหัสผ่าน');
-		window.location  = 'index.php';
-		</script>";
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-}else{
-	$_SESSION["ID"] = $objResult["ID"];
-			session_write_close();
-			if($_SESSION["ID"]==9999){
-				header("location:addmin.php");
-			}else{
-                $_SESSION["ID"] = $objResult["ID"];
-                session_write_close();
-                header("location:page1.php");
-            }
-			
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
 
-}mysqli_close($connect);
-?>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <title>Mcarcare</title>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-12 mb-5 text-center">
+                <img src="image/font-white1.2.gif" alt="Mcarcare">
+                <br>
+                <br>
+            </div>
+        </div>
+
+        <form action="Login_check.php" method="post">
+            <div class="row text-center">
+                <div class="col-sm-12 mb-3 ">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="username" id="username" aria-describedby="helpId" placeholder="ชื่อผู้ใช้">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="รหัสผ่าน">
+                    </div>
+                </div>
+
+                <div class="col-sm-12 mb-4">
+                    <button type="submit" class="btn btn-1" name="login-submit" id="login-submit">เข้าสู่ระบบ</button>
+                </div>
+            </div>
+        </form>
+        <form action="signup.php" method="post">
+            <div class="row text-center">
+                <div class="col-sm-12">
+                    <div class="d-flex justify-content-center links">
+                        หากคุณยังไม่เคยเข้าใช้งานกรุณา
+                        <a href="register.html" class="link">สมัครสมาชิก</a>
+                        &nbsp; ,คุณ
+                        <a href="forgotpassword.html">ลืมรหัสผ่าน?</a>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+
+
+</html>
