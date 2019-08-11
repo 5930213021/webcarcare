b50c7a58abc489a8e518d5dd9cb4684d7cff44d5
5930213021 <38065206+5930213021@users.noreply.github.com>
2019-08-11 20:25:08 +0700

    edit data

--- Login.php
+++ Login.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
 
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Kanit:200,300&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" type="text/css" href="css/login.css">
     <title>Mcarcare</title>
 
